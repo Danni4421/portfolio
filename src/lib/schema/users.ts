9bf25defe4678dcbb66d12f4d18d0a3c0d0f6647
Danni4421 <endaelek1234@gmail.com>
2024-11-22 22:25:59 +0700

    update: change database to serverless

--- src/lib/schema/users.ts
+++ src/lib/schema/users.ts
@@ -1,13 +1,19 @@
-import { sqliteTable as table, integer, text } from "drizzle-orm/sqlite-core";
+import {
+  pgTable as table,
+  integer,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
-export const users = table(
-    "users", 
-    {
-        id: integer().primaryKey({ autoIncrement: true }),
-        userName: text("user_name").unique(),
-        firstName: text("first_name"),
-        lastName: text("last_name"),
-        email: text().unique().notNull(),
-        password: text(),
-    }
-);
+export const users = table("users", {
+  id: integer().primaryKey(),
+  userName: text("user_name").unique(),
+  firstName: text("first_name"),
+  lastName: text("last_name"),
+  email: text().unique().notNull(),
+  password: text(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => new Date()),
+});
